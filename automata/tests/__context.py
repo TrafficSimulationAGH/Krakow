376788eba7c9be7b9319a6a11b07e26da8e86373
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-05-13 22:52:47 +0200

    Core tests implemented

--- automata/tests/__context.py
+++ automata/tests/__context.py
@@ -4,10 +4,10 @@ This module serves the package basing on its path.
 import sys
 import os
 
-def _global_path():
+def _global_path(relative):
     folder = os.path.dirname(__file__)
-    upupdir = os.path.join('..','..')
-    path = os.path.join(folder, upupdir)
+    path = os.path.join(folder, relative)
     return os.path.abspath(path)
 
-sys.path.insert(0, _global_path())
+sys.path.insert(0, _global_path(os.path.join('..','..')))
+sys.path.insert(0, _global_path('..'))
--- automata/tests/__init__.py
+++ automata/tests/__init__.py
@@ -6,6 +6,7 @@ Imports: unittest
 from unittest import main
 import __context
 
+from automata.tests.test_core import *
 from automata.tests.test_utils import *
 
 if __name__ == "__main__":
