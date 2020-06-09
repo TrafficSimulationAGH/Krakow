d6df7b5a37f4d8e1e8c23072c7de9be12e607ad4
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-06-09 10:51:35 +0200

    Stats implemented

--- automata/stats.py
+++ automata/stats.py
@@ -1,9 +1,61 @@
 """
 Statistics logging module.
 """
+import pandas as pd
+import automata.utils
 
 class Stat:
     "Statistics abstract class for logging."
 
     def __init__(self):
-        pass
+        self.cell_log = None
+        self.agent_log = None
+
+    def save(self, fpath='log.csv'):
+        if self.cell_log is not None:
+            self.cell_log.to_csv(f'cell.{fpath}', sep=';')
+        if self.agent_log is not None:
+            self.agent_log.to_csv(f'agent.{fpath}', sep=';')
+
+    def log_cells(self, cell_array):
+        "Log cells state."
+        update = [cell2dict(x) for x in cell_array]
+        if len(update) > 0:
+            if self.cell_log is None:
+                self.cell_log = pd.DataFrame(update)
+            else:
+                self.cell_log = self.cell_log.append(update, ignore_index=True)
+
+    def log_agents(self, agent_array):
+        "Log agents state."
+        update = [agent2dict(x) for x in agent_array]
+        if len(update) > 0:
+            if self.agent_log is None:
+                self.agent_log = pd.DataFrame(update)
+            else:
+                self.agent_log = self.agent_log.append(update, ignore_index=True)
+
+    def append(self, cellular):
+        "Append logs row."
+        self.log_cells(cellular.array)
+        self.log_agents(cellular.agents)
+
+def cell2dict(cell, iteration=0):
+    return {
+        'x': cell.coords[0],
+        'y': cell.coords[1],
+        'id': cell.id,
+        'lanes': cell.lanes,
+        'density': round(cell.vehicles / cell.lanes, 2),
+        'speed_lim': cell.speed_lim,
+        'type': cell.TYPE,
+        'iteration': iteration
+    }
+
+def agent2dict(agent, iteration=0):
+    return {
+        'id': agent.cell.id,
+        'v': agent.v,
+        'km/h': automata.utils.vcell2speed(agent.v),
+        'iteration': iteration
+    }
