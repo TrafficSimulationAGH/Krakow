9485027de609636446d9f90a7bc2ca2abb1b7e2a
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-06-09 14:40:04 +0200

    Upgraded tests to new version of automata

--- automata/tests/test_core.py
+++ automata/tests/test_core.py
@@ -2,18 +2,19 @@ import os
 from unittest import TestCase
 from . import mock
 import automata.core as core
+import automata.utils as utils
 
 class TestCell(TestCase):
     def test_is_free(self):
-        x = core.Cell(None)
+        x = core.Cell([0.0,0.0])
         self.assertTrue(x.is_free())
-        x.vehicle = core.Vehicle(1)
+        x.set_vehicle(core.Vehicle(1))
         self.assertFalse(x.is_free())
 
 class TestSpawnPoint(TestCase):
     def test_spawn(self):
         sp = core.SpawnPoint([0.0, 0.0])
-        sp.P = 1.0
+        sp.RATE = 1.0
         fst = sp.is_free()
         sp.spawn()
         snd = not sp.is_free()
@@ -21,7 +22,7 @@ class TestSpawnPoint(TestCase):
 
         sp.set_vehicle(None)
         fst = sp.is_free()
-        sp.P = 0.0
+        sp.RATE = 0.0
         sp.spawn()
         snd = sp.is_free()
         self.assertTrue(fst and snd)
@@ -29,8 +30,10 @@ class TestSpawnPoint(TestCase):
 class TestVehicle(TestCase):
     def test_randomize(self):
         vh = core.Vehicle(5)
-        vh.P = 1.0
-        vh.V_MAX = 6
+        vh.SLOW = 1.0
+        vh.FAST = 0.0
+        vh.LIMIT = 0.0
+        vh.V_MAX = utils.vcell2speed(6)
         vh.randomize()
         self.assertNotEqual(vh.v, 5)
         vh.v = 1
@@ -40,7 +43,9 @@ class TestVehicle(TestCase):
     def test_step(self):
         road = mock.MockStraightRoad
         car = core.Vehicle(1)
-        car.P = 0.0
+        car.SLOW = 0.0
+        car.FAST = 0.0
+        car.LIMIT = 0.0
         road.set_vehicle(car)
         car.step()
         self.assertTrue(road.is_free())
